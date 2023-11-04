d388a4017cf3c61c1b0a53ebcae1f55b6da8d48f
Miron Jakubowski <mijakubowski@gmail.com>
2023-11-04 11:30:59 +0100

    Render questions

--- quiz_app/lib/questions_screen.dart
+++ quiz_app/lib/questions_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/asnwer_button.dart';
-import 'package:quiz_app/models.dart';
+import 'package:quiz_app/data.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -13,6 +13,7 @@ class QuestionsScreen extends StatefulWidget {
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
   var questionsIndex = 0;
+  var answers = List<String>.filled(questions.length, '', growable: false);
 
   @override
   Widget build(BuildContext context) {
@@ -27,70 +28,27 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
           ),
         ),
         const SizedBox(height: 30),
-        AnswerButton('Answer 1', () {}),
-        AnswerButton('Answer 1', () {}),
-        AnswerButton('Answer 1', () {}),
+        ...mapAnswers(),
       ],
     );
   }
 
-  void answer() {
+  void answerQuestion(String answer) {
+    if (questionsIndex >= questions.length - 1) {
+      return;
+    }
+
     setState(() {
+      answers[questionsIndex] = answer;
       questionsIndex++;
     });
   }
-}
 
-const questions = [
-  QuizQuestion(
-    'What are the main building blocks of Flutter UIs?',
-    [
-      'Widgets',
-      'Components',
-      'Blocks',
-      'Functions',
-    ],
-  ),
-  QuizQuestion('How are Flutter UIs built?', [
-    'By combining widgets in code',
-    'By combining widgets in a visual editor',
-    'By defining widgets in config files',
-    'By using XCode for iOS and Android Studio for Android',
-  ]),
-  QuizQuestion(
-    'What\'s the purpose of a StatefulWidget?',
-    [
-      'Update UI as data changes',
-      'Update data as UI changes',
-      'Ignore data changes',
-      'Render UI that does not depend on data',
-    ],
-  ),
-  QuizQuestion(
-    'Which widget should you try to use more often: StatelessWidget or StatefulWidget?',
-    [
-      'StatelessWidget',
-      'StatefulWidget',
-      'Both are equally good',
-      'None of the above',
-    ],
-  ),
-  QuizQuestion(
-    'What happens if you change data in a StatelessWidget?',
-    [
-      'The UI is not updated',
-      'The UI is updated',
-      'The closest StatefulWidget is updated',
-      'Any nested StatefulWidgets are updated',
-    ],
-  ),
-  QuizQuestion(
-    'How should you update data inside of StatefulWidgets?',
-    [
-      'By calling setState()',
-      'By calling updateData()',
-      'By calling updateUI()',
-      'By calling updateState()',
-    ],
-  ),
-];
+  Iterable<AnswerButton> mapAnswers() {
+    return questions[questionsIndex]
+        .answers
+        .map((text) => AnswerButton(text, () {
+              answerQuestion(text);
+            }));
+  }
+}
